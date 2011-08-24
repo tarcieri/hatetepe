a46bc534ee82bbeb99551e1ad1dd8446cf4c94e7
Lars Gierth <lars.gierth@gmail.com>
2011-08-24 03:32:40 +0200

    Remove cruft

--- test/builder_test.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path("..", __FILE__) + "/test_helper"
-
-class BuilderTest < Test::Unit::TestCase
-  def test_bar
-    assert true
-  end
-end
--- test/parser_test.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path("..", __FILE__) + "/test_helper"
-
-class ParserTest < Test::Unit::TestCase
-  def test_foo
-    assert true
-  end
-end
