0b8034ce921049f0d59dc661e309ed3f5bdfdb8e
Lars Gierth <lars.gierth@gmail.com>
2011-10-26 23:19:48 +0200

    Add API docs for App

--- lib/hatetepe/app.rb
+++ lib/hatetepe/app.rb
@@ -9,13 +9,29 @@ module Hatetepe
   ERROR_RESPONSE = [500, {"Content-Type" => "text/html"},
                     ["Internal Server Error"]].freeze
   
+  # Interface between Rack-compatible applications and Hatetepe's server.
+  # Provides support for both synchronous and asynchronous responses.
   class App
     attr_reader :app
     
+    # Initializes a new App object.
+    #
+    # @param [#call] app
+    #   The Rack app
+    #
     def initialize(app)
       @app = app
     end
     
+    # Processes the request.
+    #
+    # Will call #postprocess with the Rack app's response. Catches :async
+    # as an additional indicator for an asynchronous response. Uses a standard
+    # 500 response if the Rack app raises an error.
+    #
+    # @param [Hash] env
+    #   The Rack environment
+    #
     def call(env)
       env["async.callback"] = proc {|response|
         postprocess env, response
@@ -28,6 +44,21 @@ module Hatetepe
       postprocess env, response
     end
     
+    # Sends the response.
+    #
+    # Does nothing if response status is indicating an asynchronous response.
+    # This is the case if the response Array's first element equals -1.
+    # Otherwise it will start sending the response (status and headers).
+    #
+    # If the body indicates streaming it will return after sending the status
+    # and headers. This happens if the body equals Rack::STREAMING. Otherwise
+    # it sends each body chunk and then closes the response stream.
+    #
+    # @param [Hash] env
+    #   The Rack environment
+    # @param [Array] response
+    #   An array of 1..3 length containing the status, headers, body
+    #
     def postprocess(env, response)
       return if response[0] == ASYNC_RESPONSE[0]
       
