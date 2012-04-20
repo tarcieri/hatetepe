357400ab225010ab9fe7e2c23a5afbe6dc938384
Lars Gierth <lars@soundcloud.com>
2012-04-20 23:54:33 +0200

    Add more CLI specs

--- spec/integration/cli/start_spec.rb
+++ spec/integration/cli/start_spec.rb
@@ -1,26 +1,36 @@
 require "spec_helper"
 require "hatetepe/cli"
 require "hatetepe/server"
+require "hatetepe/version"
 require "rack/builder"
 
 describe Hatetepe::CLI do
+  let :rackup do
+    [ proc {|*| }, {} ]
+  end
+
   before do
-    $stderr = StringIO.new
+    file = File.expand_path("config.ru")
+    Rack::Builder.stub(:parse_file).with(file) { rackup }
+
+    $stdout, $stderr = StringIO.new, StringIO.new
+    @old_env         = ENV.delete("RACK_ENV")
   end
   
   after do
-    $stderr = STDERR
+    $stdout, $stderr = STDOUT, STDERR
+    ENV["RACK_ENV"]  = @old_env
+  end
+
+  describe "#version" do
+    it "prints Hatetepe's version" do
+      Hatetepe::CLI.start([ "version" ])
+      $stdout.rewind
+      $stdout.read.should include(Hatetepe::VERSION)
+    end
   end
 
   describe "#start" do
-    let :rackup do
-      [ proc {|*| }, {} ]
-    end
-
-    before do
-      Rack::Builder.should_receive(:parse_file) { rackup }
-    end
-
     it "starts a server running the default configuration" do
       Hatetepe::Server.should_receive(:start) do |config|
         config[:host].should    == "127.0.0.1"
@@ -48,15 +58,56 @@ describe Hatetepe::CLI do
       EM.should_receive(:epoll)
       Hatetepe::CLI.start([])
     end
+
+    it "doesn't overwrite RACK_ENV" do
+      ENV["RACK_ENV"] = "foobar"
+      Hatetepe::CLI.start([])
+      ENV["RACK_ENV"].should == "foobar"
+    end
+
+    describe "with --bind option" do
+      it "passes the :host option to Server.start" do
+        Hatetepe::Server.should_receive(:start) do |config|
+          config[:host].should == "127.0.5.1"
+        end
+        Hatetepe::CLI.start([ "--bind", "127.0.5.1" ])
+      end
+    end
+
+    describe "with --port option" do
+      it "passes the :port option to Server.start" do
+        Hatetepe::Server.should_receive(:start) do |config|
+          config[:port].should == 5234
+        end
+        Hatetepe::CLI.start([ "--port", "5234" ])
+      end
+    end
+
+    describe "with --timeout option" do
+      it "passes the :timeout option to Server.start" do
+        Hatetepe::Server.should_receive(:start) do |config|
+          config[:timeout].should == 123.4
+        end
+        Hatetepe::CLI.start([ "--timeout", "123.4" ])
+      end
+    end
+
+    describe "with --rackup option" do
+      it "boots from the specified RackUp file" do
+        file = File.expand_path("./other_config.ru")
+        Rack::Builder.should_receive(:parse_file).with(file) { rackup }
+        Hatetepe::Server.should_receive(:start) do |config|
+          config[:app].should equal(rackup[0])
+        end
+        Hatetepe::CLI.start([ "--rackup", "other_config.ru" ])
+      end
+    end
+
+    describe "with --env option" do
+      it "sets RACK_ENV to the specified value" do
+        Hatetepe::CLI.start([ "--env", "production" ])
+        ENV["RACK_ENV"].should == "production"
+      end
+    end
   end
-
-  describe "with --bind option"
-
-  describe "with --port option"
-
-  describe "with --rackup option"
-
-  describe "with --env option"
-
-  describe "with --timeout option"
 end
